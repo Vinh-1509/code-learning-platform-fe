15a18830811069df967933dfed2b213274dc45d5
TQD1911 <dotrungquan.1911@gmail.com>
2026-04-26 14:42:11 +0700

    test

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,14 +1,24 @@
 import axios from 'axios';
 
 const api = axios.create({
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   baseURL: import.meta.env.VITE_API_URL,
   headers: { 'Content-Type': 'application/json' },
 });
 
 api.interceptors.response.use(
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   (response) => response.data,
-  (error) => {
-    if (error.response?.status === 401) return Promise.reject(error);
+  (error: unknown) => {
+    if (axios.isAxiosError(error)) {
+      if (error.response?.status === 401) {
+        return Promise.reject(error);
+      }
+    }
+
+    return Promise.reject(
+      error instanceof Error ? error : new Error('Unknown error')
+    );
   }
 );
 
