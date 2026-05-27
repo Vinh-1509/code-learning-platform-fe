8d7cfdc477cb21613d2faab978115bc4543a24f2
kinus_is_coding <stasianery123@gmail.com>
2026-05-27 22:25:46 +0700

    fix: language slection page+ guardrouting

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,42 @@
 import { redirect } from '@tanstack/react-router';
+import { getMe } from './axios';
 
 export const getAccessToken = (): string | null => {
-  if (typeof window === 'undefined') return null;
   return localStorage.getItem('token');
 };
 
-export const requireAuth = () => {
+export const requireAuth = async () => {
   const token = getAccessToken();
+
   if (!token) {
-    return redirect({
-      to: '/login',
+    throw redirect({ to: '/login' });
+  }
+
+  let user;
+
+  try {
+    user = await getMe();
+  } catch {
+    localStorage.removeItem('token');
+    throw redirect({ to: '/login' });
+  }
+
+  if (!user || !user.selectedLanguage || user.selectedLanguage.length === 0) {
+    throw redirect({ to: '/languageselection' });
+  }
+};
+export const checkLanguageSelection = async () => {
+  const token = getAccessToken();
+
+  if (!token) {
+    throw redirect({ to: '/login' });
+  }
+
+  const user = await getMe();
+
+  if (user?.selectedLanguage && user.selectedLanguage.length > 0) {
+    throw redirect({
+      to: '/dashboard',
     });
   }
 };
