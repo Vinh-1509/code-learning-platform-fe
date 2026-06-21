d38365d655371f3b7533b070e223a39e99820b44
kinus_is_coding <stasianery123@gmail.com>
2026-06-21 23:29:05 +0700

    fix: config playwirhgt

--- playwright.config.ts
+++ playwright.config.ts
@@ -75,9 +75,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:5173',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'yarn dev --host 0.0.0.0',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120000,
+  },
 });
